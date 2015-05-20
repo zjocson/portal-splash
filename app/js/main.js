0b138addb9f081dca28fb3a86ec308931a9d8358
Zaida Jocson <Zaida_Jocson@discovery.com>
2015-05-20 14:31:56 -0400

    Add in portal example, resize

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,66 @@
-var loadIframe = function() {
-    var iframeContainer = document.createElement('div');
-    var splashIframe = document.createElement('iframe');
-    var windowHeight = window.innerHeight;
-    //splashIframe.src = 'http://cnn.com';
-    splashIframe.src = 'http://s3.amazonaws.com/static.agency.discovery.com/portal-splash/test.html';
-    splashIframe.setAttribute('id', 'splash-iframe')
-    iframeContainer.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100%; height:' + windowHeight + 'px;');
-    splashIframe.setAttribute('style', 'width: 100%; height:' + windowHeight + 'px;');
-    document.body.appendChild(iframeContainer);
-    iframeContainer.appendChild(splashIframe);
-}
-
-
-function setSplashCookie () {
-    if (document.cookie.replace(/(?:(?:^|.*;\s*)splashCookie\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
-        loadIframe();
-        document.cookie = "splashCookie=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+var xSetPage = (function () {
+  //  MAKE SETTING ATTRIBUTES QUICKER
+  var setAttributes = function (el, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
     }
- }
- setSplashCookie();
+  };
 
- function receiveCloseMessage(event){
-   if (event.data=="removeIframe"){
-      var element = document.getElementById('splash-iframe');
-      element.parentNode.removeChild(element);
-   }
-}
-window.addEventListener('message', receiveCloseMessage, false);
+  //  LOAD THE SPLASH PAGE IFRAME AND ADD TO BODY
+  var loadIframe = function() {
+      var xWindowHeight = window.innerHeight,
+          xWindowWidth = window.innerWidth,
+          iframeContainer = document.createElement('div'),
+          splashIframe = document.createElement('iframe');
+
+      setAttributes(splashIframe, {
+        'id': 'splash-iframe',  
+        'src':'http://s3.amazonaws.com/static.agency.discovery.com/portal-splash/test.html', 
+        'style':'width: 100%; height:' + xWindowHeight + 'px;' 
+      });
+      setAttributes(iframeContainer, {
+        'id': 'splash-container',
+        'style': 'position: absolute; top: 0; left: 0; width: 100%; height:' + xWindowHeight + 'px;'
+      });
+      
+      document.body.appendChild(iframeContainer);
+      iframeContainer.appendChild(splashIframe);
+  };
+
+  // COOKIE FOR SPLASH PAGE, SHOW ONCE
+  var setSplashCookie = (function () {
+      if (document.cookie.replace(/(?:(?:^|.*;\s*)splashCookie\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+          loadIframe();
+          document.cookie = "splashCookie=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+      }
+   })();
+
+  // RECEIVE MESSAGE FROM WITHIN IFRAME 
+  var receiveCloseMessage = function(event) {
+     if (event.data=='removeIframe'){
+        var splashFrame = document.getElementById('splash-iframe');
+        splashFrame.parentNode.removeChild(splashFrame);
+     }
+  }
+
+  // RESIZE STUFF
+  var setSizes = function () {
+    var newHeight = window.innerHeight,
+        newWidth = window.innerWidth,
+        splashFrame = document.getElementById('splash-iframe'),
+        splashContainer = document.getElementById('splash-container');
+    splashContainer.setAttribute('style', 'width: 100%; height:' + newHeight + 'px;');
+    splashFrame.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100%; height:' + newHeight + 'px;');
+    
+  }
+
+  return {
+    xReceiveCloseMessage : receiveCloseMessage,
+    xSetSizes : setSizes 
+  };
+
+})();
+
+
+window.addEventListener('message', xSetPage.xReceiveCloseMessage, false);
+window.addEventListener('resize', xSetPage.xSetSizes);
