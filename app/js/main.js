be223418f092a55f9663abfe730751f2c11ea588
Zaida Jocson <Zaida_Jocson@discovery.com>
2015-05-20 16:40:01 -0400

    Formatting

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,18 @@
 var xSetPage = (function () {
-  //  MAKE SETTING ATTRIBUTES QUICKER
-  var setAttributes = function (el, attrs) {
-    for(var key in attrs) {
-      el.setAttribute(key, attrs[key]);
-    }
-  };
 
   //  LOAD THE SPLASH PAGE IFRAME AND ADD TO BODY
-  var loadIframe = function() {
-      var xWindowHeight = window.innerHeight,
-          xWindowWidth = window.innerWidth,
+  var loadIframe = function () {
+        var xWindowHeight = window.innerHeight,
           iframeContainer = document.createElement('div'),
           splashIframe = document.createElement('iframe');
-
-      setAttributes(splashIframe, {
-        'id': 'splash-iframe',  
-        'src':'http://s3.amazonaws.com/static.agency.discovery.com/portal-splash/test.html', 
-        'style':'width: 100%; height:' + xWindowHeight + 'px;' 
-      });
-      setAttributes(iframeContainer, {
-        'id': 'splash-container',
-        'style': 'position: absolute; top: 0; left: 0; width: 100%; height:' + xWindowHeight + 'px;'
-      });
-      
-      document.body.appendChild(iframeContainer);
-      iframeContainer.appendChild(splashIframe);
+        document.body.appendChild(iframeContainer);
+        iframeContainer.appendChild(splashIframe);
+        splashIframe.src = 'http://s3.amazonaws.com/static.agency.discovery.com/portal-splash/test.html';
+        splashIframe.setAttribute('style', 'width: 100%; height:' + xWindowHeight + 'px;');
+        splashIframe.id = 'splash-iframe';
+        iframeContainer.id = 'splash-container';
+        iframeContainer.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100%; height:' + xWindowHeight + 'px;');
   };
-
   // COOKIE FOR SPLASH PAGE, SHOW ONCE
   var setSplashCookie = (function () {
       if (document.cookie.replace(/(?:(?:^|.*;\s*)splashCookie\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
@@ -37,30 +23,50 @@ var xSetPage = (function () {
 
   // RECEIVE MESSAGE FROM WITHIN IFRAME 
   var receiveCloseMessage = function(event) {
-     if (event.data=='removeIframe'){
+     if (event.data==='removeIframe'){
         var splashFrame = document.getElementById('splash-iframe');
         splashFrame.parentNode.removeChild(splashFrame);
      }
-  }
+  };
 
-  // RESIZE STUFF
-  var setSizes = function () {
+  // BASIC DEBOUNCE
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  //RESIZE STUFF
+  var setSizes = debounce(function () {
     var newHeight = window.innerHeight,
-        newWidth = window.innerWidth,
         splashFrame = document.getElementById('splash-iframe'),
         splashContainer = document.getElementById('splash-container');
     splashContainer.setAttribute('style', 'width: 100%; height:' + newHeight + 'px;');
-    splashFrame.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100%; height:' + newHeight + 'px;');
-    
-  }
+    splashFrame.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100%; height:' + newHeight + 'px;' );
+  }, 250);
 
   return {
-    xReceiveCloseMessage : receiveCloseMessage,
-    xSetSizes : setSizes 
+    xReceiveCloseMessage: receiveCloseMessage,
+    xSetSizes: setSizes 
   };
 
 })();
 
 
 window.addEventListener('message', xSetPage.xReceiveCloseMessage, false);
-window.addEventListener('resize', xSetPage.xSetSizes);
+window.addEventListener('resize', function () {
+  if (document.getElementById('splash-container') !== null ) {
+    xSetPage.xSetSizes();
+  } else {
+    console.log('resized');
+  }
+});
